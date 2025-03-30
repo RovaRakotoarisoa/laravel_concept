d0d21fc339224dffffa935cf2710ce1874a3a3f1
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-03-30 21:59:32 +0300

    create all Model Policy Seeders Factory Controller

---
+++ app/Models/Job.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Job extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobFactory> */
+    use HasFactory;
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+}
