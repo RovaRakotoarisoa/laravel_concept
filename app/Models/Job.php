6bb58239a9adf8bf06b813344c401ae4242fe24e
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-04-02 13:02:51 +0300

    all

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -13,6 +13,18 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    public function tag(string $name)
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+
+        $this->tags()->attach($tag);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     public function employer()
     {
         return $this->belongsTo(Employer::class);
---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Job;
+
+//test = it
+
+
+//AAA
+
+//Arange
+//Act
+//Assert
+
+it('it belongs to an employer', function () {
+   
+    //Arange
+
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    //Act and Assert
+
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+
+it('can have tags', function(){
+    //Arange
+    $job = Job::factory()->create();
+
+    //Act 
+    $job->tag('Frontend');
+
+    //Assert
+    expect($job->tags)->toHaveCount(1);
+});
