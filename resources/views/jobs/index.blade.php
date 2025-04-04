b1d074c5298a536cd693647f7e947a0a10eedc59
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-04-04 13:50:28 +0300

    delete welcome.blade.php

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -13,9 +13,9 @@
             <x-section-heading> Featured Job</x-section-heading> 
             
             <div class="grid lg:grid-cols-3 gap-5 mt-6">
-                <x-job-card />
-                <x-job-card />
-                <x-job-card />
+                @foreach($featuredJobs as $job)
+                    <x-job-card :$job />
+                @endforeach
             </div>
         </section>
 
@@ -23,16 +23,11 @@
             <x-section-heading>Tags</x-section-heading>
 
             <div class="mt-6 space-x-1">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
+
+                @foreach($tags as $tag)
+                {{-- :tag="$tag" Equal to :$tag if props is the same of variable --}}
+                    <x-tag :$tag />
+                @endforeach
             </div>
 
         </section>
@@ -41,9 +36,9 @@
             <x-section-heading> Recent Jobs </x-section-heading>
 
             <div class="mt-6 space-y-6">
-                <x-job-card-wide />
-                <x-job-card-wide />
-                <x-job-card-wide />
+                @foreach($jobs as $job)
+                    <x-job-card-wide :$job />
+                @endforeach
             </div>
             
         </section>
