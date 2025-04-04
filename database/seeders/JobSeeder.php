19748bb92b905ad9a524714bc354fd6f5f6013e3
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-04-04 13:54:50 +0300

    call models(Job and Tag and Sequence) and create factory with relation in hasAttached

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
+use App\Models\Job;
+use App\Models\Tag;
 
 class JobSeeder extends Seeder
 {
@@ -12,6 +18,17 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tags = Tag::factory(3)->create();
+
+        Job::factory(20)->hasAttached($tags)->create(new Sequence([
+                'featured' => false,
+                'schedule' => 'Full Time'
+
+            ],[
+                'featured' => true,
+                'schedule' => 'Part Time'
+
+            ],
+        ));
     }
 }
